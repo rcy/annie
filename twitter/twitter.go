4b105d5f82e438461d24083305d0f4703d92cca4
Ryan Yeske <ryan@ryanyeske.com>
2023-08-28 21:50:26 -0700

    Better twitter error handling

--- twitter/twitter.go
+++ twitter/twitter.go
@@ -12,8 +12,11 @@ func Post(text string) error {
 	nvurl := os.Getenv("NICHE_VOMIT_URL")
 	if nvurl != "" {
 		res, err := http.Post(nvurl, "text/plain", strings.NewReader(text))
-		if res.StatusCode >= 300 || err != nil {
-			return errors.New(fmt.Sprintf("error posting to twitter %d %v\n", res.StatusCode, err))
+		if err != nil {
+			return errors.New(fmt.Sprintf("error posting to twitter err=%s\n", err))
+		}
+		if res.StatusCode >= 300 {
+			return errors.New(fmt.Sprintf("error posting to twitter statusCode=%s\n", err))
 		}
 	}
 	return nil
