bc1314ccdfc81d24957dcc08acf1b048fbf8bc8c
Ryan Yeske <ryan@ryanyeske.com>
2023-09-12 20:12:05 -0700

    Store quotes delivered privately

--- handlers/quote.go
+++ handlers/quote.go
@@ -6,16 +6,15 @@ import (
 )
 
 func Quote(params bot.HandlerParams) error {
-	if params.Target == params.Nick {
-		params.Privmsgf(params.Target, "not your personal secretary")
-		return nil
-	}
-
 	text := params.Matches[1]
 
 	err := notes.Create(params.Target, params.Nick, "quote", text)
 	if err != nil {
 		return err
+	}
+
+	if params.Target == params.Nick {
+		params.Privmsgf(params.Target, "stored quote to share later, maybe")
 	}
 
 	return nil
