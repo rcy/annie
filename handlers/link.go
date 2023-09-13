517f8c01a288106da573233840151719524336e6
Ryan Yeske <ryan@ryanyeske.com>
2023-09-12 20:03:59 -0700

    Store links posted in private messages

--- handlers/link.go
+++ handlers/link.go
@@ -3,26 +3,21 @@ package handlers
 import (
 	"goirc/bot"
 	"goirc/model/notes"
-	"log"
 )
 
 func Link(params bot.HandlerParams) error {
 	url := params.Matches[1]
-
-	if params.Target == params.Nick {
-		// posted in a private message
-		params.Privmsgf(params.Target, "this will be shared later")
-		return nil
-	}
-
-	// posted to channel
 
 	err := notes.Create(params.Target, params.Nick, "link", url)
 	if err != nil {
 		return err
 	}
 
-	log.Printf("recorded url %s", url)
+	if params.Target == params.Nick {
+		// posted in a private message
+		params.Privmsgf(params.Target, "stored link to share later, maybe")
+		return nil
+	}
 
 	return nil
 }
