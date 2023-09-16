92ab3ae0d255ce1a67af7310fe7db7d398f06fd3
Ryan Yeske <ryan@ryanyeske.com>
2023-09-15 17:15:11 -0700

    Update feedme to pull from private notes

--- handlers/feed_me.go
+++ handlers/feed_me.go
@@ -1,17 +1,30 @@
 package handlers
 
 import (
+	"database/sql"
 	"goirc/bot"
 	"goirc/model"
 	"goirc/model/notes"
+	"goirc/util"
 )
 
 func FeedMe(params bot.HandlerParams) error {
-	rows := []notes.Note{}
+	note := notes.Note{}
 
-	err := model.DB.Select(&rows, `select id, created_at, nick, text, kind from notes order by random() limit 1`)
+	err := model.DB.Get(&note, `select id, created_at, nick, text, kind from notes where nick = target order by random() limit 1`)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			params.Privmsgf(params.Target, "no")
+			return nil
+		}
+		return err
+	}
+	_, err = model.DB.Exec(`update notes set target = ? where id = ?`, params.Target, note.Id)
 	if err != nil {
 		return err
 	}
+
+	params.Privmsgf(params.Target, "%s (from ??? %s ago)", note.Text, util.Since(note.CreatedAt))
+
 	return nil
 }
