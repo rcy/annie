23997990bc6ce7a86362696c147a77a47032d38f
Ryan Yeske <ryan@ryanyeske.com>
2024-07-28 16:23:25 -0700

    Fix tests

--- handlers/weather/weather_test.go
+++ handlers/weather/weather_test.go
@@ -13,31 +13,31 @@ func TestWeather(t *testing.T) {
 	}{
 		{
 			filename: "testdata/chicago,us.json",
-			want:     "Chicago, United States -6.8°C (feels like -11.2°C), snow, mist, 1.1mm pcpn over last hour, visibility 2.0km, wind 9 kph wsw",
+			want:     "-6.8°C (feels like -11.2°C), snow, mist, 1.1mm pcpn over last hour, visibility 2.0km, wind 9 kph wsw",
 		},
 		{
 			filename: "testdata/creston,ca.json",
-			want:     "Creston, Canada -10.9°C, overcast clouds, visibility 6.0km, wind 4 kph nne (gust 4 kph)",
+			want:     "-10.9°C, overcast clouds, visibility 6.0km, wind 4 kph nne (gust 4 kph)",
 		},
 		{
 			filename: "testdata/gibsons,ca.json",
-			want:     "Gibsons, Canada -0.6°C (feels like -4.9°C), overcast clouds, wind 14 kph nne (gust 16 kph)",
+			want:     "-0.6°C (feels like -4.9°C), overcast clouds, wind 14 kph nne (gust 16 kph)",
 		},
 		{
 			filename: "testdata/shanghai,cn.json",
-			want:     "Shanghai, China 9.2°C (feels like 7.5°C), moderate rain, 1.9mm pcpn over last hour, visibility 7.0km, wind 11 kph n",
+			want:     "9.2°C (feels like 7.5°C), moderate rain, 1.9mm pcpn over last hour, visibility 7.0km, wind 11 kph n",
 		},
 		{
 			filename: "testdata/toronto,ca.json",
-			want:     "Toronto, Canada -8.1°C (feels like -15.1°C), overcast clouds, wind 30 kph wsw (gust 39 kph)",
+			want:     "-8.1°C (feels like -15.1°C), overcast clouds, wind 30 kph wsw (gust 39 kph)",
 		},
 		{
 			filename: "testdata/victoria,ca.json",
-			want:     "Victoria, Canada 0.6°C (feels like -5.9°C), overcast clouds, wind 32 kph n",
+			want:     "0.6°C (feels like -5.9°C), overcast clouds, wind 32 kph n",
 		},
 		{
 			filename: "testdata/vancouver,ca.json",
-			want:     "Vancouver, Canada -1.7°C (feels like -5.2°C), overcast clouds, wind 9 kph e",
+			want:     "-1.7°C (feels like -5.2°C), overcast clouds, wind 9 kph e",
 		},
 	} {
 		t.Run(tc.filename, func(t *testing.T) {
