fb24df2f45757df2990e45b56c360cb8e96df254
Ryan Yeske <ryan@ryanyeske.com>
2024-09-24 16:56:19 -0700

    Anonymize links based on environment variable

--- db/model/notes.go
+++ db/model/notes.go
@@ -7,9 +7,13 @@ import (
 )
 
 func (n Note) Link() (string, error) {
-	str, err := idstr.Encode(n.ID)
-	if err != nil {
-		return "", err
+	if os.Getenv("ANONYMIZE_LINKS") != "" {
+		str, err := idstr.Encode(n.ID)
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("%s/%s", os.Getenv("ROOT_URL"), str), nil
 	}
-	return fmt.Sprintf("%s/%s", os.Getenv("ROOT_URL"), str), nil
+
+	return n.Text.String, nil
 }
