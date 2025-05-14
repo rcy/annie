2e5040acf292178da4c1c3c72c2f71ee696ee7c6
Ryan Yeske <ryan@ryanyeske.com>
2025-05-13 23:50:34 -0700

    Consider users timezone when reporting the date

--- handlers/ddate/ddate.go
+++ handlers/ddate/ddate.go
@@ -1,15 +1,26 @@
 package ddate
 
 import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
 	"goirc/bot"
+	"goirc/db/model"
+	db "goirc/model"
 	"os"
 	"os/exec"
 	"strings"
 )
 
 func Handle(params bot.HandlerParams) error {
+	tz, err := getNickTimezone(context.TODO(), params.Nick)
+	if err != nil {
+		return fmt.Errorf("getNickTimezone: %w", err)
+	}
+
 	cmd := exec.Command("ddate")
-	cmd.Env = append(os.Environ(), "TZ=America/Creston")
+	cmd.Env = append(os.Environ(), "TZ="+tz)
 
 	var out strings.Builder
 	cmd.Stdout = &out
@@ -23,4 +34,16 @@ func Handle(params bot.HandlerParams) error {
 	params.Privmsgf(params.Target, "%s", str)
 
 	return nil
+}
+
+func getNickTimezone(ctx context.Context, nick string) (string, error) {
+	q := model.New(db.DB.DB)
+	nickTimezone, err := q.GetNickTimezone(ctx, nick)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return "America/Los_Angeles", nil
+		}
+		return "", fmt.Errorf("GetNickTimezone: %w", err)
+	}
+	return nickTimezone.Tz, nil
 }
