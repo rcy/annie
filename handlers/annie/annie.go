d74ac20944b58b1eb15951ee2ded95aec0524918
Ryan Yeske <ryan@ryanyeske.com>
2025-07-25 13:48:37 -0700

    Handle requests

--- handlers/annie/annie.go
+++ handlers/annie/annie.go
@@ -25,7 +25,7 @@ func Handle(params bot.HandlerParams) error {
 
 	q := model.New(db.DB.DB)
 
-	response, err := ai.Complete(ctx, openai.GPT4oMini, "categorize input into statements, questions or pleasantries.  If it is a statement, reply with the one word 'statement'.  If it is a question, reply with 'question'.  If it is a pleasantry, reply with 'pleasantry'", msg)
+	response, err := ai.Complete(ctx, openai.GPT4oMini, "categorize input into statements, questions, requests, or pleasantries.  If it is a statement, reply with the one word 'statement'.  If it is a question, reply with 'question'.  If it is a request reply with 'request', if it is a pleasantry, reply with 'pleasantry'", msg)
 	if err != nil {
 		return err
 	}
@@ -61,6 +61,31 @@ func Handle(params bot.HandlerParams) error {
 You are annie, a friend hanging out in an irc channel.
 The current time and date is %s.
 You have been asked a question. Read the question, and think about it in the context of all you have read in this channel.
+Respond with single sentences, in lower case, with minimal punctuation.
+Do not refer to yourself in the third person.
+`, time.Now().Format(time.RFC1123))
+
+		systemPrompt += strings.Join(lines, "\n")
+
+		response, err := ai.Complete(ctx, openai.GPT4oMini, systemPrompt, msg)
+		if err != nil {
+			return err
+		}
+		params.Privmsgf(params.Target, "%s: %s", params.Nick, response)
+	case "request":
+		notes, err := q.NonAnonNotes(ctx)
+		if err != nil {
+			return err
+		}
+		lines := make([]string, len(notes))
+		for i, n := range notes {
+			lines[i] = fmt.Sprintf("%s <%s> %s", n.CreatedAt, n.Nick.String, n.Text.String)
+		}
+
+		systemPrompt := fmt.Sprintf(`
+You are annie, a friend hanging out in an irc channel.
+The current time and date is %s.
+You have been given a request. Read the request, and think about it in the context of all you have read in this channel.
 Respond with single sentences, in lower case, with minimal punctuation.
 Do not refer to yourself in the third person.
 `, time.Now().Format(time.RFC1123))
